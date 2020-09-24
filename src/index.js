f7266224b09a4195c56b8bc1966dcd210bb6b461
AlenaGoncharova <alen.gon4arowa@gmail.com>
2020-09-25 02:49:57 +0300

    Create the skeleton application

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
-/* eslint no-console: "error" */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
-// custom console
-console.log("Hello world!");
+import App from './components/app';
+import ErrorBoundary from './components/error-boundary';
+// import AviasalesService from './services/aviasales-service';
+import DummyAviasalesService from './services/dummy-aviasales-service';
+import { AviasalesServiceProvider } from './components/aviasales-service-context';
+
+import store from './store';
+
+// const aviasalesService = new AviasalesService();
+const aviasalesService = new DummyAviasalesService();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ErrorBoundary>
+      <AviasalesServiceProvider value={aviasalesService}>
+        <App></App>
+      </AviasalesServiceProvider>
+    </ErrorBoundary>
+  </Provider>,
+  document.getElementById('root'),
+);
