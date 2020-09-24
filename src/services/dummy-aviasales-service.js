9c1879fb1cc6a7e61238c8a915ea30c70a7dc6a3
AlenaGoncharova <alen.gon4arowa@gmail.com>
2020-09-25 02:33:32 +0300

    Create service for test data

---
+++ src/services/dummy-aviasales-service.js
@@ -0,0 +1,31 @@
+export default class DummyAviasalesService {
+  getAllTickets() {
+    return [
+      {
+        price: 3000,
+        carrier: 7,
+        segments: [],
+      },
+      {
+        price: 10000,
+        carrier: 1,
+        segments: [],
+      },
+      {
+        price: 6500,
+        carrier: 7,
+        segments: [],
+      },
+      {
+        price: 3000,
+        carrier: 9,
+        segments: [],
+      },
+      {
+        price: 4000,
+        carrier: 7,
+        segments: [],
+      },
+    ];
+  }
+}
