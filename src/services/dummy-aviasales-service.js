279856583c160c9f7904366a58d62ae46602e8fa
AlenaGoncharova <alen.gon4arowa@gmail.com>
2020-09-28 21:14:33 +0300

    Add asynchronous work with test data

--- src/services/dummy-aviasales-service.js
+++ src/services/dummy-aviasales-service.js
@@ -1,106 +1,112 @@
 export default class DummyAviasalesService {
+  data = [
+    {
+      price: 3000,
+      carrier: 7,
+      segments: [
+        {
+          origin: 'MOW',
+          destination: 'HKT',
+          date: new Date(2020, 8, 26, 10, 45),
+          stops: ['HKG', 'JNB'],
+          duration: 1275,
+        },
+        {
+          origin: 'HKT',
+          destination: 'MOW',
+          date: new Date(2020, 8, 28, 11, 20),
+          stops: ['HKG'],
+          duration: 810,
+        },
+      ],
+    },
+    {
+      price: 10000,
+      carrier: 1,
+      segments: [
+        {
+          origin: 'MOW',
+          destination: 'HKT',
+          date: new Date(2020, 8, 26, 10, 45),
+          stops: ['HKG', 'JNB'],
+          duration: 1275,
+        },
+        {
+          origin: 'HKT',
+          destination: 'MOW',
+          date: new Date(2020, 8, 28, 11, 20),
+          stops: ['HKG'],
+          duration: 810,
+        },
+      ],
+    },
+    {
+      price: 6500,
+      carrier: 7,
+      segments: [
+        {
+          origin: 'MOW',
+          destination: 'HKT',
+          date: new Date(2020, 8, 26, 10, 45),
+          stops: ['HKG', 'JNB'],
+          duration: 1275,
+        },
+        {
+          origin: 'HKT',
+          destination: 'MOW',
+          date: new Date(2020, 8, 28, 11, 20),
+          stops: ['HKG'],
+          duration: 810,
+        },
+      ],
+    },
+    {
+      price: 3000,
+      carrier: 9,
+      segments: [
+        {
+          origin: 'MOW',
+          destination: 'HKT',
+          date: new Date(2020, 8, 26, 10, 45),
+          stops: ['HKG', 'JNB'],
+          duration: 1275,
+        },
+        {
+          origin: 'HKT',
+          destination: 'MOW',
+          date: new Date(2020, 8, 28, 11, 20),
+          stops: ['HKG'],
+          duration: 810,
+        },
+      ],
+    },
+    {
+      price: 4000,
+      carrier: 7,
+      segments: [
+        {
+          origin: 'MOW',
+          destination: 'HKT',
+          date: new Date(2020, 8, 26, 10, 45),
+          stops: ['HKG', 'JNB'],
+          duration: 1275,
+        },
+        {
+          origin: 'HKT',
+          destination: 'MOW',
+          date: new Date(2020, 8, 28, 11, 20),
+          stops: ['HKG'],
+          duration: 810,
+        },
+      ],
+    },
+  ];
+
   getAllTickets() {
-    return [
-      {
-        price: 3000,
-        carrier: 7,
-        segments: [
-          {
-            origin: 'MOW',
-            destination: 'HKT',
-            date: new Date(2020, 8, 26, 10, 45),
-            stops: ['HKG', 'JNB'],
-            duration: 1275,
-          },
-          {
-            origin: 'HKT',
-            destination: 'MOW',
-            date: new Date(2020, 8, 28, 11, 20),
-            stops: ['HKG'],
-            duration: 810,
-          },
-        ],
-      },
-      {
-        price: 10000,
-        carrier: 1,
-        segments: [
-          {
-            origin: 'MOW',
-            destination: 'HKT',
-            date: new Date(2020, 8, 26, 10, 45),
-            stops: ['HKG', 'JNB'],
-            duration: 1275,
-          },
-          {
-            origin: 'HKT',
-            destination: 'MOW',
-            date: new Date(2020, 8, 28, 11, 20),
-            stops: ['HKG'],
-            duration: 810,
-          },
-        ],
-      },
-      {
-        price: 6500,
-        carrier: 7,
-        segments: [
-          {
-            origin: 'MOW',
-            destination: 'HKT',
-            date: new Date(2020, 8, 26, 10, 45),
-            stops: ['HKG', 'JNB'],
-            duration: 1275,
-          },
-          {
-            origin: 'HKT',
-            destination: 'MOW',
-            date: new Date(2020, 8, 28, 11, 20),
-            stops: ['HKG'],
-            duration: 810,
-          },
-        ],
-      },
-      {
-        price: 3000,
-        carrier: 9,
-        segments: [
-          {
-            origin: 'MOW',
-            destination: 'HKT',
-            date: new Date(2020, 8, 26, 10, 45),
-            stops: ['HKG', 'JNB'],
-            duration: 1275,
-          },
-          {
-            origin: 'HKT',
-            destination: 'MOW',
-            date: new Date(2020, 8, 28, 11, 20),
-            stops: ['HKG'],
-            duration: 810,
-          },
-        ],
-      },
-      {
-        price: 4000,
-        carrier: 7,
-        segments: [
-          {
-            origin: 'MOW',
-            destination: 'HKT',
-            date: new Date(2020, 8, 26, 10, 45),
-            stops: ['HKG', 'JNB'],
-            duration: 1275,
-          },
-          {
-            origin: 'HKT',
-            destination: 'MOW',
-            date: new Date(2020, 8, 28, 11, 20),
-            stops: ['HKG'],
-            duration: 810,
-          },
-        ],
-      },
-    ];
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(this.data);
+      }, 800);
+    });
   }
 }
