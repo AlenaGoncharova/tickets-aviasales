8212a68b83a4fc586ce15940526c088a1aa7be4a
AlenaGoncharova <alen.gon4arowa@gmail.com>
2020-09-28 20:29:35 +0300

    Add test data

--- src/services/dummy-aviasales-service.js
+++ src/services/dummy-aviasales-service.js
@@ -4,27 +4,102 @@ export default class DummyAviasalesService {
       {
         price: 3000,
         carrier: 7,
-        segments: [],
+        segments: [
+          {
+            origin: 'MOW',
+            destination: 'HKT',
+            date: new Date(2020, 8, 26, 10, 45),
+            stops: ['HKG', 'JNB'],
+            duration: 1275,
+          },
+          {
+            origin: 'HKT',
+            destination: 'MOW',
+            date: new Date(2020, 8, 28, 11, 20),
+            stops: ['HKG'],
+            duration: 810,
+          },
+        ],
       },
       {
         price: 10000,
         carrier: 1,
-        segments: [],
+        segments: [
+          {
+            origin: 'MOW',
+            destination: 'HKT',
+            date: new Date(2020, 8, 26, 10, 45),
+            stops: ['HKG', 'JNB'],
+            duration: 1275,
+          },
+          {
+            origin: 'HKT',
+            destination: 'MOW',
+            date: new Date(2020, 8, 28, 11, 20),
+            stops: ['HKG'],
+            duration: 810,
+          },
+        ],
       },
       {
         price: 6500,
         carrier: 7,
-        segments: [],
+        segments: [
+          {
+            origin: 'MOW',
+            destination: 'HKT',
+            date: new Date(2020, 8, 26, 10, 45),
+            stops: ['HKG', 'JNB'],
+            duration: 1275,
+          },
+          {
+            origin: 'HKT',
+            destination: 'MOW',
+            date: new Date(2020, 8, 28, 11, 20),
+            stops: ['HKG'],
+            duration: 810,
+          },
+        ],
       },
       {
         price: 3000,
         carrier: 9,
-        segments: [],
+        segments: [
+          {
+            origin: 'MOW',
+            destination: 'HKT',
+            date: new Date(2020, 8, 26, 10, 45),
+            stops: ['HKG', 'JNB'],
+            duration: 1275,
+          },
+          {
+            origin: 'HKT',
+            destination: 'MOW',
+            date: new Date(2020, 8, 28, 11, 20),
+            stops: ['HKG'],
+            duration: 810,
+          },
+        ],
       },
       {
         price: 4000,
         carrier: 7,
-        segments: [],
+        segments: [
+          {
+            origin: 'MOW',
+            destination: 'HKT',
+            date: new Date(2020, 8, 26, 10, 45),
+            stops: ['HKG', 'JNB'],
+            duration: 1275,
+          },
+          {
+            origin: 'HKT',
+            destination: 'MOW',
+            date: new Date(2020, 8, 28, 11, 20),
+            stops: ['HKG'],
+            duration: 810,
+          },
+        ],
       },
     ];
   }
