e6bcc655d19f5024a39ced9ce9da484dd1d31cc3
AlenaGoncharova <alen.gon4arowa@gmail.com>
2020-09-28 20:34:58 +0300

    Refactoring the component to work with the added data

--- src/components/ticket-list-item/ticket-list-item.js
+++ src/components/ticket-list-item/ticket-list-item.js
@@ -1,12 +1,20 @@
 import React, { Fragment } from 'react';
 
+import TicketSegmentDetails from '../ticket-segment-details';
+
 const TicketListItem = ({ ticket }) => {
-  const { price, carrier} = ticket;
+  const { price, carrier, segments} = ticket;
+  const segmentTo = segments[0];
+  const segmentBack = segments[1];
   return (
-    <Fragment>
-      <span>{price}</span>
-      <span>{carrier}</span>
-    </Fragment>
+    <div className="ticket-list-item">
+      <div>
+        <span className="ticket-price">{price}</span>
+        <span className="ticket-carrier"> {carrier}</span>
+      </div>
+      <div className="ticket-segment-to"><TicketSegmentDetails segment={segmentTo}/></div>
+      <div className="ticket-segment-back"><TicketSegmentDetails segment={segmentBack}/></div>
+    </div>
   );
 };
 
