18244f2d399743bd3b5f1c46ceac785d2c484152
AlenaGoncharova <alen.gon4arowa@gmail.com>
2020-10-04 00:59:50 +0300

    Connect the sort component to the store

--- src/components/sorting/sorting.js
+++ src/components/sorting/sorting.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { sortTypeChanged } from '../../actions';
 
-const Sorting = () => {
+const Sorting = ({ sortType, sortTypeChanged }) => {
+  const handleClickSortButton = (newSortType) => {
+    if (newSortType !== sortType) {
+      sortTypeChanged(newSortType);
+    }
+  };
+
   return (
     <div>
-      <button>САМЫЙ ДЕШЕВЫЙ</button>
-      <button>САМЫЙ БЫСТРЫЙ</button>
+      <span>{sortType}</span>
+      <button onClick={() => handleClickSortButton('price')}>САМЫЙ ДЕШЕВЫЙ</button>
+      <button onClick={() => handleClickSortButton('time')}>САМЫЙ БЫСТРЫЙ</button>
     </div>
   );
 };
 
-export default Sorting;
+const mapStateToProps = ({ sortType }) => {
+  return {
+    sortType,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    sortTypeChanged: (sortType) => {
+      dispatch(sortTypeChanged(sortType));
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Sorting);
