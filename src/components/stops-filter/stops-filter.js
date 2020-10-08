0b1538893a6a750fdfaada8712f3f8ee4044c61c
AlenaGoncharova <alen.gon4arowa@gmail.com>
2020-10-09 00:48:31 +0300

    Split component StopsFilter into presentational and container

--- src/components/stops-filter/stops-filter.js
+++ src/components/stops-filter/stops-filter.js
@@ -3,44 +3,15 @@ import { connect } from 'react-redux';
 
 import { stopsFilterChanged } from '../../actions';
 
-const StopsFilter = ({ filterByStops, stopsFilterChanged }) => {
-  const isCheckedStopsFilter = (countOfStops) => {
-    return filterByStops.has(countOfStops);
-  };
-
-  const handleChangeStopsFilter = (countOfStops) => {
-    const newFilterByStops = new Set(filterByStops);
-    if (newFilterByStops.has(countOfStops)) {
-      newFilterByStops.delete(countOfStops);
-    } else {
-      newFilterByStops.add(countOfStops);
-    }
-
-    stopsFilterChanged(newFilterByStops);
-  };
-
-  const allStopsFilter = [
-    { countOfStops: 0, name: 'withoutStops', text: 'Без пересадок' },
-    { countOfStops: 1, name: 'oneStop', text: '1 пересадка' },
-    { countOfStops: 2, name: 'twoStops', text: '2 пересадки' },
-    { countOfStops: 3, name: 'threeStops', text: '3 пересадки' },
-  ];
-
-  const handleChangeAllStopsFilter = () => {
-    if (filterByStops.size === allStopsFilter.length) {
-      stopsFilterChanged(new Set([]));
-    } else {
-      stopsFilterChanged(new Set(allStopsFilter.map((filter) => { return filter.countOfStops; })));
-    }
-  };
-
+const StopsFilter = ({ allStopsFilter, isCheckedAllStopsFilter, handleChangeAllStopsFilter,
+                      isCheckedStopsFilter, handleChangeStopsFilter }) => {
   return (
     <div>
       <legend>КОЛИЧЕСТВО ПЕРЕСАДОК</legend>
       <div>
         <input type="checkbox" value="allStopsFilter"
-                checked={filterByStops.size === allStopsFilter.length}
-                onChange={() => { handleChangeAllStopsFilter(); }} />
+                checked={isCheckedAllStopsFilter()}
+                onChange={handleChangeAllStopsFilter} />
         <label htmlFor="allStopsFilter">Все</label>
       </div>
       {
@@ -62,6 +33,51 @@ const StopsFilter = ({ filterByStops, stopsFilterChanged }) => {
   );
 };
 
+const StopsFilterContainer = ({ filterByStops, stopsFilterChanged }) => {
+  const allStopsFilter = [
+    { countOfStops: 0, name: 'withoutStops', text: 'Без пересадок' },
+    { countOfStops: 1, name: 'oneStop', text: '1 пересадка' },
+    { countOfStops: 2, name: 'twoStops', text: '2 пересадки' },
+    { countOfStops: 3, name: 'threeStops', text: '3 пересадки' },
+  ];
+
+  const isCheckedStopsFilter = (countOfStops) => {
+    return filterByStops.has(countOfStops);
+  };
+
+  const handleChangeStopsFilter = (countOfStops) => {
+    const newFilterByStops = new Set(filterByStops);
+    if (newFilterByStops.has(countOfStops)) {
+      newFilterByStops.delete(countOfStops);
+    } else {
+      newFilterByStops.add(countOfStops);
+    }
+
+    stopsFilterChanged(newFilterByStops);
+  };
+
+  const isCheckedAllStopsFilter = () => {
+    return filterByStops.size === allStopsFilter.length;
+  };
+
+  const handleChangeAllStopsFilter = () => {
+    if (filterByStops.size === allStopsFilter.length) {
+      stopsFilterChanged(new Set([]));
+    } else {
+      stopsFilterChanged(new Set(allStopsFilter.map((filter) => { return filter.countOfStops; })));
+    }
+  };
+
+  return (
+    <StopsFilter
+      allStopsFilter={allStopsFilter}
+      isCheckedAllStopsFilter={isCheckedAllStopsFilter}
+      handleChangeAllStopsFilter={handleChangeAllStopsFilter}
+      isCheckedStopsFilter={isCheckedStopsFilter}
+      handleChangeStopsFilter={handleChangeStopsFilter} />
+  );
+};
+
 const mapStateToProps = ({ filterByStops }) => {
   return {
     filterByStops,
@@ -76,4 +92,4 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(StopsFilter);
+export default connect(mapStateToProps, mapDispatchToProps)(StopsFilterContainer);
